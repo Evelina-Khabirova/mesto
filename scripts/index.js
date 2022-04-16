788084b475c12f4ea51a88d805d3537072170391
Khabirova Evelina <evelina.khabirova@yandex.ru>
2022-04-16 21:02:37 +0300

    refactor: добавить правки по замечаниям код-ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
-const saveButton = popup.querySelector('.popup__save');
 let profileFullname = document.querySelector('.profile__fullname');
 let profileAboutMe = document.querySelector('.profile__about-me');
 let fullname = popup.querySelector('.popup__input_type_fullname');
@@ -18,7 +17,7 @@ function closePopup() {
   popup.classList.remove('popup__active');
 }
 
-function changeInformation() {
+function changeInformation(event) {
   profileFullname.textContent = fullname.value;
   profileAboutMe.textContent = aboutMe.value;
   event.preventDefault();
